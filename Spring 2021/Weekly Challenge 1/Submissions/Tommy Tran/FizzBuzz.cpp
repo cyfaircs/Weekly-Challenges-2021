9dd0e18a8d4b172527d3931fbe8df285a3fcc408
ShadowStorm0 <41564329+ShadowStorm0@users.noreply.github.com>
2021-10-05 02:33:04 -0500

    Reorganization and Addition of Else Statement
    Signed-off-by: ShadowStorm0 <41564329+ShadowStorm0@users.noreply.github.com>

--- Spring 2021/Weekly Challenge 1/Submissions/Tommy Tran/FizzBuzz.cpp
+++ Spring 2021/Weekly Challenge 1/Submissions/Tommy Tran/FizzBuzz.cpp
@@ -3,27 +3,24 @@
     Date Created: 2/24/2020                     */
 
 #include <iostream>
-
 using namespace std;
 
-int main() {
+int main(){
 
-    for (int i = 1; i <= 100; i++) {
+    for (int i = 1; i <= 100; i++){
 
-        //Multiple by 3 and 5
-        if (i % 15 == 0) {
+        if (i % 15 == 0){ //Multiple by 3 and 5
             cout << i << " - FizzBuzz\n";
         }
-
-        //Multiple by 3
-        else if ((i % 3) == 0) {
+        else if ((i % 3) == 0){ //Multiple by 3
             cout << i << " - Fizz\n";
         }
-
-        //Multiple by 5
-        else if ((i % 5) == 0) {
+        else if ((i % 5) == 0){ //Multiple by 5
             cout << i << " - Buzz\n";
         }
+        else{ //Return i if not divisible
+            cout << i << " \n";
+        }
     }
-    return 0;
+return 0;
 }
